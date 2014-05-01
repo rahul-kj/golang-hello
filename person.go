9c8ba2c900a653b1de210dd1f18f97a8a4815214
Jain <jainr4@usxxjainr4m5.corp.emc.com>
2014-04-30 23:04:29 -0500

    second commit

---
+++ person.go
@@ -0,0 +1,49 @@
+package hello
+
+import "fmt"
+
+// Define a structure type called Person
+type Person struct {
+   FName string
+   LName string
+   Age int
+}
+
+type Persons []Person
+
+// Function that updates the values of the passed in pointer of the variable
+func UpdatePerson(person *Person) {
+   fmt.Println(person.FName, person.LName, person.Age)
+
+   person.Age = 40
+   fmt.Println(person.FName, person.LName, person.Age)
+}
+
+func (person *Person) Rename(fName string) {
+	person.FName = fName
+}
+
+func (persons Persons) BranchingExample() {
+	// Use len function to get the size of the slice/array
+	fmt.Println("Length of the persons slice", len(persons))
+
+	if persons[1].FName == "Gerry" {
+		fmt.Println("Yes Gerry is here")
+	} else {
+		fmt.Println("Oops Gerry is not here")
+	}
+
+	// Conditional case which has no condition on switch, but has on case that returns a boolean
+	for _, p := range persons {
+		switch {
+		case p.Age <= 20 :
+			fmt.Println(p.FName, "belongs to Group 1")
+		case p.Age <= 30, p.Age > 40 :
+			fmt.Println(p.FName, "belongs to Group 2")
+		case p.Age <= 40 :
+			fmt.Println(p.FName, "belongs to Group 3")
+		default :
+			fmt.Println(p.FName, "belongs to Group X")
+		}
+	}
+}
